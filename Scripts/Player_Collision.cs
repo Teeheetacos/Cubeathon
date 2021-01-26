18605516854e321831869a59c446382fcf9b80e6
Teeheetacos <1987675+Teeheetacos@users.noreply.github.com>
2021-01-26 15:45:31 -0700

    Add files via upload

---
+++ Scripts/Player_Collision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player_Collision : MonoBehaviour
+{
+
+    public Player_Movement movement;
+
+    void OnCollisionEnter (Collision collision_Info)
+    {
+        if (collision_Info.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+
+}
